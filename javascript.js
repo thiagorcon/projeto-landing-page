f195d9934071377ede0e79ca071ef380baaf5228
Thiago Rosa <thiagorcon@hotmail.com>
2023-10-19 17:39:41 -0300

    envio 03 correcao formulario e criacao de opagina produtos

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
-let campoNovo = document.createElement('div')
-let dadosNome = document.getElementById('idNome')
-let dadosSobreNome = document.getElementById('idSobrenome')
-let textoCadastro = document.createTextNode (`Prezado(a) ${dadosNome} ${dadosSobreNome} , ficamos muito grato com seu cadastro em nosso site. Aproveite nossas promoções e dicas de alimentação. Siga-nos na nossas redes socias.`)
-let texto = campoNovo.appendChild(textoCadastro)
+//let nome =  document.getElementById('idNome')
+     // let texto = document.getElementById('idMeuCadastro')
+     // texto.innerHTML =  nome.value  
 
-texto.insertAdjacentElement('beforebegin',campoNovo)
-let teste = document.getElementById('idMeuCadastro')
-campoNovo =  teste.innerHTML
+
+function enviar(event) {
+     event.preventDefault() 
+     let campoNovo = document.createElement('div')
+     let dadosNome = document.getElementById('idNome').value
+     let dadosSobreNome = document.getElementById('idSobrenome').value
+     let expressao = document.getElementById('idExpressao').value
+     let textoCadastro = document.createTextNode(`Prezado(a) ${expressao} ${dadosNome} ${dadosSobreNome} , ficamos muito grato com seu cadastro em nosso site. Aproveite nossas promoções e dicas de alimentação. Siga-nos na nossas redes socias.`)
+     
+     campoNovo.appendChild(textoCadastro)
+  
+     let teste = document.getElementById('idMeuCadastro')
+     teste.insertAdjacentElement('beforeend', campoNovo)
+         
+}
 
